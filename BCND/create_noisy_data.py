e0fe8ee58741884c3a4513af3da2690ad204e585
returaj <returaj.burnwal@gmail.com>
2023-12-27 12:59:57 +0530

    create noisy data

---
+++ BCND/create_noisy_data.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python3
+
+import os
+import json
+import jax
+
+
+def collect_data(key, expert_data, noise_data, ep):
+    num_episode = len(expert_data)
+    uniform_samples = jax.random.uniform(key, shape=(num_episode,))
+    mix_data = []
+    for i, z in enumerate(uniform_samples):
+        if z > ep:
+            mix_data.append(noise_data[i])
+        else:
+            mix_data.append(expert_data[i])
+    return mix_data
+
+
+def load_data(path):
+    with open(path, "r") as f:
+        d = json.load(f)
+    return d
+
+
+def save_data(path, data):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    path = f"{path}/trajectories.json"
+    with open(path, "w") as f:
+        json.dump(data, f)
+
+
+def main(expert_data_path, noise_data_path, save_path, ep, seed):
+    expert_data = load_data(expert_data_path)
+    noise_data = load_data(noise_data_path)
+    mix_data = collect_data(
+        key=jax.random.PRNGKey(seed),
+        expert_data=expert_data,
+        noise_data=noise_data,
+        ep=ep
+    )
+
+    save_path = f"{save_path}/{ep}"
+    save_data(save_path, mix_data)
+
+
+if __name__ == '__main__':
+    env_name = "ant"
+    ep = 0.5
+
+    name_and_seed = [("random", 0), ("normal", 1), ("zero", 2)]
+
+    current_file_path = os.path.dirname(__file__)
+    data_path = f"{current_file_path}/../trajectories/data"
+    expert_data_path = f"{data_path}/{env_name}/expert/trajectories.json"
+
+    for noise_name, seed in name_and_seed:
+        noise_data_path = f"{data_path}/{env_name}/{noise_name}/trajectories.json"
+        save_data_path = f"{current_file_path}/noisy_data/{env_name}/expert-{noise_name}"
+        main(
+            expert_data_path=expert_data_path,
+            noise_data_path=noise_data_path,
+            save_path=save_data_path,
+            ep=ep,
+            seed=seed
+        )
+
